212f5b08318e6a20261216d971e7d84ce916905a
ʙᴏʙʙʏ <109764682+love-u-bobby@users.noreply.github.com>
2022-07-29 20:12:36 +0530

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -27,5 +27,5 @@ if '%errorlevel%' NEQ '0' (
 :--------------------------------------
 :: your commands begin from this point.
 :: stops the service and then starts it 
-net stop angel,-x-robot
+net stop angel-x-robot
 net start angel-x-robot
