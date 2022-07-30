abcf3ac004d38ad34865a62e394c47713b19bdca
ʙᴏʙʙʏ <109764682+love-u-bobby@users.noreply.github.com>
2022-07-30 16:06:53 +0530

    Update notes.py

--- AngelxRobot/modules/notes.py
+++ AngelxRobot/modules/notes.py
@@ -3,14 +3,14 @@ from io import BytesIO
 import random
 from typing import Optional
 
-import FallenRobot.modules.sql.notes_sql as sql
-from FallenRobot import LOGGER, JOIN_LOGGER, SUPPORT_CHAT, dispatcher, DRAGONS
-from FallenRobot.modules.disable import DisableAbleCommandHandler
-from FallenRobot.modules.helper_funcs.handlers import MessageHandlerChecker
-from FallenRobot.modules.helper_funcs.chat_status import user_admin, connection_status
-from FallenRobot.modules.helper_funcs.misc import build_keyboard, revert_buttons
-from FallenRobot.modules.helper_funcs.msg_types import get_note_type
-from FallenRobot.modules.helper_funcs.string_handling import (
+import AngelxRobot.modules.sql.notes_sql as sql
+from AngelxRobot import LOGGER, JOIN_LOGGER, SUPPORT_CHAT, dispatcher, DRAGONS
+from AngelxRobot.modules.disable import DisableAbleCommandHandler
+from AngelxRobot.modules.helper_funcs.handlers import MessageHandlerChecker
+from AngelxRobot.modules.helper_funcs.chat_status import user_admin, connection_status
+from AngelxRobot.modules.helper_funcs.misc import build_keyboard, revert_buttons
+from AngelxRobot.modules.helper_funcs.msg_types import get_note_type
+from AngelxRobot.modules.helper_funcs.string_handling import (
     escape_invalid_curly_brackets,
 )
 from telegram import (
@@ -542,7 +542,7 @@ A button can be added to a note by using standard markdown link syntax - the lin
 
 """
 
-__mod_name__ = "Nᴏᴛᴇs"
+__mod_name__ = "Nᴏᴛᴇs📝"
 
 GET_HANDLER = CommandHandler("get", cmd_get)
 HASH_GET_HANDLER = MessageHandler(Filters.regex(r"^#[^\s]+"), hash_get)
