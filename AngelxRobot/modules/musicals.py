cd51b3849be0101280be68a897a3add59812d974
ʙᴏʙʙʏ <109764682+love-u-bobby@users.noreply.github.com>
2022-07-30 16:00:46 +0530

    Update musicals.py

--- AngelxRobot/modules/musicals.py
+++ AngelxRobot/modules/musicals.py
@@ -1,10 +1,10 @@
-__mod_name__ = "Mᴜsɪᴄ"
+__mod_name__ = "Mᴜsɪᴄ🎧"
 
 __help__ = """
  ❍ /song <song name>*:* 𝙐𝙥𝙡𝙤𝙖𝙙𝙨 𝙩𝙝𝙚 𝙨𝙤𝙣𝙜 𝙞𝙣 𝙞𝙩'𝙨 𝙗𝙚𝙨𝙩 𝙦𝙪𝙖𝙡𝙞𝙩𝙮 𝙖𝙫𝙖𝙞𝙡𝙖𝙗𝙡𝙚
  💡Ex: `/song Faded Alan Walker`
 𝙄 𝘾𝘼𝙉 𝙋𝙇𝘼𝙔 𝙈𝙐𝙎𝙄𝘾 𝙏𝙊𝙊
 MY MUSIC COMMANDS ??? 
-HERE : https://t.me/WILDBOTUPDATES/7
+HERE : https://t.me/comrade_robotz
 
 """
