12f3bee2e2a357035d81b531b7bcf6833052100d
ʙᴏʙʙʏ <109764682+love-u-bobby@users.noreply.github.com>
2022-07-30 16:40:15 +0530

    Update tiny.py

--- AngelxRobot/modules/tiny.py
+++ AngelxRobot/modules/tiny.py
@@ -1,8 +1,8 @@
 import os
 import cv2
 from PIL import Image
-from FallenRobot.events import register
-from FallenRobot import telethn as tbot
+from AngelxRobot.events import register
+from AngelxRobot import telethn as tbot
 
 
 @register(pattern="^/tiny ?(.*)")
@@ -77,7 +77,7 @@ async def _(event):
     os.remove(ik)
 
 
-__mod_name__ = "Tɪɴʏ"
+__mod_name__ = "Tɪɴʏ◽"
 __help__ = """
 ❍ /tiny*:* reply a sticker and see magic
 """
