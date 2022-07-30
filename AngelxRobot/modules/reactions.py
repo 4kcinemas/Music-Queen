5d45d33b241b945b61d4666d3ddb9875c0229675
ʙᴏʙʙʏ <109764682+love-u-bobby@users.noreply.github.com>
2022-07-30 16:19:54 +0530

    Update reactions.py

--- AngelxRobot/modules/reactions.py
+++ AngelxRobot/modules/reactions.py
@@ -1,7 +1,7 @@
 import random
 
-from FallenRobot import dispatcher
-from FallenRobot.modules.disable import DisableAbleCommandHandler
+from AngelxRobot import dispatcher
+from AngelxRobot.modules.disable import DisableAbleCommandHandler
 from telegram import Update
 from telegram.ext import CallbackContext, run_async
 
